60945e96f0d27c06154b2c1508a79dc57b45ad57
wiihoffmann <wiihoffmann@yahoo.ca>
2022-08-25 16:16:27 -0600

    add pin for VAS slider

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@
 #define E_STOP_INTERRUPT_PIN 3
 #define SOLENOID_PIN 10
 #define VACUUM_PIN 11
+#define VAS_SLIDER_PIN A0
 
 Adafruit_ADS1115 ads; /* Use this for the 16-bit version */
 // Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
